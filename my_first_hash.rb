06468f93f5ced4ae3ea76e44ceca99a86b61d6b9
natashabhuta <natasha.bhuta@gmail.com>
2018-08-08 21:47:57 +0000

    automatically backed up by learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -32,9 +32,9 @@ def adding
                   "oil paintings" => 3
                  }
                  
-  shipping_manifest = ["muskets" => 2]
-  # add 2 muskets to the shipping_manifest hash below
 
+  # add 2 muskets to the shipping_manifest hash below
+  shipping_manifest = ["muskets" => 2]
 
 
   # add 4 gun powder to the shipping_manifest hash below
