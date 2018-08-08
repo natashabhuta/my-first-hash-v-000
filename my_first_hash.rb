a25e8d1adb80a2fb5dc36f87d3427993e160044f
natashabhuta <natasha.bhuta@gmail.com>
2018-08-08 21:46:36 +0000

    automatically backed up by learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -22,7 +22,7 @@ def retrieval
                  }
 
   # Look up the value of the "oil paintings" key in the shipping_manifest hash below
-
+shipping_manifest["oil paintings"]
 end
 
 def adding
@@ -43,6 +43,6 @@ shipping_manifest = ["gun powder" => 4]
 
   # return the shipping_manifest hash below
   
-shipping_manifest["oil paintings"]
+shipping_manifest
 
 end
